fae4d7001978b0788ef0f4765f03c8bd603a42d9
andr-ew <andrewcshike@gmail.com>
2023-06-19 15:30:08 -0500

    div, gate_len, stride: refactor to params

--- arqueggiator.lua
+++ arqueggiator.lua
@@ -1,18 +1,17 @@
 local arqueggiator = {}
 arqueggiator.__index = arqueggiator 
 
-function arqueggiator.new()
+function arqueggiator.new(id)
     local self = {}
     setmetatable(self, arqueggiator)
+
+    self.id = id or '1'
 
     self.sequence = {}
     self.step = 1
     self.gate = 0
+    self.index = 1
     
-    self.div = 1/2
-    self.gate_length = 0.5
-    self.stride = 1
-
     self.clk = 0
 
     self.action_on = function(idx) end
@@ -23,11 +22,35 @@ function arqueggiator.new()
     return self
 end
 
---TODO: refactor some internal variables to params for psetfullness
-function arqueggiator:params()
+function arqueggiator:pfix(name)
+    return 'arqueggiator_'..self.id..'_'..string.gsub(name, ' ', '_')
 end
 
-local function advance(self)
+local divs = { 4/1, 3/1, 2/1, 1/1, 1/2, 1/3, 1/4, 1/5, 1/6, 1/7, 1/8 }
+local div_names = { '4/1', '3/1', '2/1', '1/1', '1/2', '1/3', '1/4', '1/5', '1/6', '1/7', '1/8' }
+
+local directions = { -1, 1 }
+local direction_names = { 'reverse', 'forward' }
+
+local cs = require 'controlspec'
+
+--TODO: refactor some internal variables to params for psetfullness
+function arqueggiator:params()
+    params:add{
+        type = 'option', id = self:pfix('division'), name = 'division',
+        options = div_names, default = tab.key(divs, 1/2),
+    }
+    params:add{
+        type = 'control', id = self:pfix('gate length'), name = 'gate length',
+        controlspec = cs.def{ min = 0, max = 80, default = 50, units = '%' }
+    }
+    params:add{
+        type = 'option', id = self:pfix('direction'), name = 'direction',
+        options = direction_names, default = tab.key(directions, 1), wrap = true,
+    }
+end
+
+local function advance(self, gate_length, stride)
     local idx = self.sequence[self.step]
 
     if idx then 
@@ -35,29 +58,41 @@ local function advance(self)
         self.gate = 1
         crops.dirty.grid = true
 
-        clock.sleep(self.gate_length * clock.get_beat_sec() * self.div)
+        clock.sleep(gate_length)
 
         if idx > 0 then self.action_off(idx) end
         self.gate = 0
         crops.dirty.grid = true
         
-        self.step = util.wrap(self.step + self.stride, 1, #self.sequence)
+        self.step = util.wrap(self.step + stride, 1, #self.sequence)
     else
         self.step = 1
     end
+
+    self.index = idx or 1
 end
 
 function arqueggiator:pulse()
-    clock.run(advance, self)
+    local div = divs[params:get(self:pfix('division'))]
+    local gate_length = params:get(self:pfix('gate length'))/100 * clock.get_beat_sec() * div
+    local stride = directions[params:get(self:pfix('direction'))]
+
+    clock.run(advance, self, gate_length, stride)
 end
 
 function arqueggiator:start()
     self.running = true
     clock.run(function()
         while self.running do 
-            clock.sync(self.div)
+            local div = divs[params:get(self:pfix('division'))]
+            local gate_length = params:get(self:pfix('gate length'))/100 
+                                * clock.get_beat_sec() 
+                                * div
+            local stride = directions[params:get(self:pfix('direction'))]
 
-            advance(self) 
+            clock.sync(div)
+
+            advance(self, gate_length, stride) 
         end
     end)
 end
