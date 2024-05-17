1abd744b3fc180eaf1122a985ff5da3e0758d9d2
andr-ew <andrewcshike@gmail.com>
2024-05-17 00:09:26 -0500

    division: lowest value is stop, changes happen immedietly

--- arqueggiator.lua
+++ arqueggiator.lua
@@ -1,58 +1,17 @@
 local arqueggiator = {}
 arqueggiator.__index = arqueggiator 
 
-function arqueggiator.new(id)
-    local self = {}
-    setmetatable(self, arqueggiator)
+local STOPPED = 1
+local divs = { 
+    4, 4/1, 2/1, 1/1, 1/2, 1/3, 1/4, 1/5, 1/6, 1/7, 1/8, 1/16, 1/32 
+}
+local div_names = { 
+    'stop', '4/1', '2/1', '1/1', '1/2', '1/3', '1/4', '1/5', '1/6', '1/7', '1/8', '1/16', '1/32'
+}
 
-    self.id = id or '1'
 
-    self.sequence = {}
-    self.step = 1
-    self.gate = 0
-    self.index = 1
-    
-    self.clk = 0
 
-    self.action_on = function(idx) end
-    self.action_off = function(idx) end
-
-    self.running = false
-
-    return self
-end
-
-function arqueggiator:pfix(name)
-    return 'arqueggiator_'..self.id..'_'..string.gsub(name, ' ', '_')
-end
-
-local divs = { 4/1, 3/1, 2/1, 1/1, 1/2, 1/3, 1/4, 1/5, 1/6, 1/7, 1/8 }
-local div_names = { '4/1', '3/1', '2/1', '1/1', '1/2', '1/3', '1/4', '1/5', '1/6', '1/7', '1/8' }
 local reverses = { [0] = 1, [1] = -1 }
-
-local cs = require 'controlspec'
-
-arqueggiator.params_count = 4
-
-function arqueggiator:params()
-    params:add{
-        type = 'option', id = self:pfix('division'), name = 'division',
-        options = div_names, default = tab.key(divs, 1/2),
-    }
-    params:add{
-        type = 'control', id = self:pfix('gate length'), name = 'gate length',
-        controlspec = cs.def{ min = 0, max = 80, default = 50, units = '%' }
-    }
-    params:add{
-        type = 'binary', behavior = 'toggle', 
-        id = self:pfix('reverse'), name = 'reverse',
-    }
-    params:add{
-        type = 'binary', behavior = 'toggle', 
-        id = self:pfix('loop'), name = 'loop', default = 1,
-        action = function() self:start() end
-    }
-end
 
 local function advance(self, gate_length, stride, loop)
     local idx = self.sequence[self.step]
@@ -81,6 +40,79 @@ local function advance(self, gate_length, stride, loop)
     self.index = idx or 1
 end
 
+function arqueggiator.new(id)
+    local self = {}
+    setmetatable(self, arqueggiator)
+
+    self.id = id or '1'
+
+    self.sequence = {}
+    self.step = 1
+    self.gate = 0
+    self.index = 1
+    
+    self.clk = 0
+
+    self.action_on = function(idx) end
+    self.action_off = function(idx) end
+
+    self.running = false
+
+    self.tick = function()
+        while self.running do 
+            local div = divs[params:get(self:pfix('division'))]
+            local gate_length = params:get(self:pfix('gate length'))/100 
+                                * clock.get_beat_sec() 
+                                * div
+            local stride = reverses[params:get(self:pfix('reverse'))]
+            local loop = params:get(self:pfix('loop')) > 0
+
+            clock.sync(div)
+
+            if self.running then advance(self, gate_length, stride, loop) end
+        end
+    end
+
+    return self
+end
+
+function arqueggiator:pfix(name)
+    return 'arqueggiator_'..self.id..'_'..string.gsub(name, ' ', '_')
+end
+
+local cs = require 'controlspec'
+
+arqueggiator.params_count = 4
+
+function arqueggiator:params()
+    params:add{
+        type = 'option', id = self:pfix('division'), name = 'division',
+        options = div_names, default = tab.key(divs, 1/2),
+        action = function(v)
+            self.running = v ~= STOPPED
+
+            clock.cancel(self.clk)
+            self.clk = clock.run(self.tick)
+        end
+    }
+    params:add{
+        type = 'control', id = self:pfix('gate length'), name = 'gate length',
+        controlspec = cs.def{ min = 0, max = 80, default = 50, units = '%' }
+    }
+    params:add{
+        type = 'binary', behavior = 'toggle', 
+        id = self:pfix('reverse'), name = 'reverse',
+    }
+    params:add{
+        type = 'binary', behavior = 'toggle', 
+        id = self:pfix('loop'), name = 'loop', default = 1,
+        action = function() self:start() end
+    }
+    -- params:add{
+    --     -- type = 'bina'
+    -- }
+end
+
 function arqueggiator:pulse()
     local div = divs[params:get(self:pfix('division'))]
     local gate_length = params:get(self:pfix('gate length'))/100 * clock.get_beat_sec() * div
@@ -93,20 +125,7 @@ end
 function arqueggiator:start()
     if not self.running then
         self.running = true
-        clock.run(function()
-            while self.running do 
-                local div = divs[params:get(self:pfix('division'))]
-                local gate_length = params:get(self:pfix('gate length'))/100 
-                                    * clock.get_beat_sec() 
-                                    * div
-                local stride = reverses[params:get(self:pfix('reverse'))]
-                local loop = params:get(self:pfix('loop')) > 0
-
-                clock.sync(div)
-
-                advance(self, gate_length, stride, loop)
-            end
-        end)
+        self.clk = clock.run(self.tick)
     end
 end
 
